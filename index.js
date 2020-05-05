f702bb1bf54b2031d4a2928d68be05067b07bdc9
priyankajoc0 <42170143+priyankajoc0@users.noreply.github.com>
2020-05-05 16:44:46 +0530

    1. uploaded

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6 +1 );   //1-6
+var randomDiceImage= "dice"+randomNumber1+ ".png";   //dice1.png - dice6.png
+var randomImageSource= "images/" +randomDiceImage;  //images/dice1.png - images/dice6.png
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6 +1 );   //1-6
+var randomDiceImage= "dice"+randomNumber2+ ".png";   //dice1.png - dice6.png
+var randomImageSource= "images/" +randomDiceImage;  //images/dice1.png - images/dice6.png
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
+
+
+if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML="Draw";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML="Player2 Winns ";
+}
+else{
+  document.querySelector("h1").innerHTML=" Player1 Winns";
+}
